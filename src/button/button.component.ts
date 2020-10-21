d3a6297ab44c2e1d24b6242d27d3072276d2c56e
Francisco barros da silva <fran@MacBook-Pro-de-Francisco.local>
2020-10-20 21:58:07 -0300

    base library

---
+++ src/button/button.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'button',
+  template: './button.component.html',
+})
+export class LibButtonComponent {}
---
+++ src/button/button.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import { LibButtonComponent } from './button.component';
+
+@NgModule({
+  imports: [LibButtonComponent],
+  declarations: [LibButtonComponent],
+  exports: []
+})
+export class LibButtonModule {}
