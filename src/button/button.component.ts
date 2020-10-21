59aa43dcfa62ce8a52273c41e9037f57cb5236ad
Francisco <franciscobarros754@gmail.com>
2020-10-20 23:48:15 -0300

    change templateUrl in component

--- src/button/button.component.ts
+++ src/button/button.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'lib-button',
-  template: './button.component.html',
+  templateUrl: './button.component.html',
 })
 export class LibButtonComponent {}
